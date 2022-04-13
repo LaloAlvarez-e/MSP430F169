25c24f319288639623b327873a721b760b4a50bc
InDeviceMex <indevicemex@gmail.com>
2022-04-13 12:52:19 -0500

    Actualizar lnk_msp430f169.cmd, main.c y msp430f169_startup.c

---
+++ MSP430F169_TIMER_SERVO/main.c
@@ -0,0 +1,145 @@
+#include "DriverLib/DriverLib.h"
+
+
+uint16_t MAIN_u16WDTInterval(uintptr_t ptrBlock, uint8_t u8Source);
+uint16_t MAIN_u16SWITCH1(uintptr_t ptrBlock, uint8_t u8Source);
+
+#define SWITCH1_PORT (GPIO_enPORT1)
+#define SWITCH1_PIN (GPIO_enPIN_NUMBER4)
+
+#define LED1_PORT (GPIO_enPORT1)
+#define LED1_PIN (GPIO_enPIN_NUMBER0)
+#define LED2_PORT (GPIO_enPORT1)
+#define LED2_PIN (GPIO_enPIN_NUMBER1)
+#define LED3_PORT (GPIO_enPORT1)
+#define LED3_PIN (GPIO_enPIN_NUMBER2)
+#define LED4_PORT (GPIO_enPORT1)
+#define LED4_PIN (GPIO_enPIN_NUMBER3)
+#define TEST_PORT (GPIO_enPORT3)
+#define TEST_PIN (GPIO_enPIN_NUMBER4)
+
+/**
+ * main.c
+ */
+uint16_t u16WDTCount = 0U;
+uint16_t u16DegreeBase = 850U;
+uint16_t u16Degree = 0U;
+#define MAX_DEGREE_VALUE (1200U)
+#define MAX_DEGREE_PORCENTAGE (100U)
+#define DEGREE_INCREASE (MAX_DEGREE_VALUE/MAX_DEGREE_PORCENTAGE)
+
+void main(void)
+{
+    CLOCK_nINT_STATUS enClockStatus = CLOCK_enINT_STATUS_NOOCCUR;
+    uint16_t u16Iter = 0U;
+    WDT_Config_t stWDTConfig =
+    {
+        WDT_enENABLE_RUN,
+        WDT_enINT_ENABLE_ENA,
+        WDT_enMODE_INTERVAL,
+        WDT_enCLOCK_ACLK,
+        WDT_enINTERVAL_512,
+    };
+
+    TIMERA_ConfigExt_t stTIMERAConfig =
+    {
+        TIMERA_enCLOCK_SMCLK,
+        TIMERA_enCLOCK_DIV_8,
+        TIMERA_enMODE_UP,
+        TIMERA_enINT_ENABLE_DIS,
+        TIMERA_enINT_STATUS_NOOCCUR,
+    };
+
+    TIMERA_Compare_ConfigExt_t stCC1Config =
+    {
+        u16DegreeBase - 1U,
+        TIMERA_enCC_COMPAREMODE_RESET_SET,
+        TIMERA_enCC_STATE_HIGH,
+        TIMERA_enINT_ENABLE_DIS,
+        TIMERA_enINT_STATUS_NOOCCUR,
+    };
+
+    SVS->CTL_bits.VLD = 0U;
+    WDT__vSetEnable(WDT_enENABLE_STOP); /*  stop watchdog timer*/
+
+    GPIO__vSetDirectionByNumber(LED1_PORT, LED1_PIN, GPIO_enDIR_OUTPUT);
+    GPIO__vSetSelectionByNumber(LED1_PORT, LED1_PIN, GPIO_enSEL_IO);
+    GPIO__vSetDirectionByNumber(LED2_PORT, LED2_PIN, GPIO_enDIR_OUTPUT);
+    GPIO__vSetSelectionByNumber(LED2_PORT, LED2_PIN, GPIO_enSEL_IO);
+    GPIO__vSetDirectionByNumber(LED3_PORT, LED3_PIN, GPIO_enDIR_OUTPUT);
+    GPIO__vSetSelectionByNumber(LED3_PORT, LED3_PIN, GPIO_enSEL_IO);
+    GPIO__vSetDirectionByNumber(LED4_PORT, LED4_PIN, GPIO_enDIR_OUTPUT);
+    GPIO__vSetSelectionByNumber(LED4_PORT, LED4_PIN, GPIO_enSEL_IO);
+    GPIO__vSetDirectionByNumber(TEST_PORT, TEST_PIN, GPIO_enDIR_OUTPUT);
+    GPIO__vSetSelectionByNumber(TEST_PORT, TEST_PIN, GPIO_enSEL_IO);
+    GPIO__vSetDirectionByNumber(SWITCH1_PORT, SWITCH1_PIN, GPIO_enDIR_INPUT);
+    GPIO__vSetSelectionByNumber(SWITCH1_PORT, SWITCH1_PIN, GPIO_enSEL_IO);
+    GPIO__vSetConfig(GPIO_enTA_OUT1_P23);
+
+    /** Rosc*/
+    GPIO__vSetConfig(GPIO_enROSC);
+
+    CLOCK__vSetDCOFrequency(800000UL, CLOCK_enRESISTOR_EXTERNAL);
+    CLOCK__vSetLFXT1FrequencyMode(CLOCK_enFREQMODE_LOW);
+    CLOCK__vSetXT2Enable(CLOCK_enENABLE_ENA);
+	do
+	{
+	    CLOCK__vClearInterruptSource();
+	    for(u16Iter = 0U; u16Iter < 4000U; u16Iter++); /*At least 50us*/
+	    enClockStatus = CLOCK__enGetStatusInterruptSource();
+	}while(CLOCK_enINT_STATUS_NOOCCUR != enClockStatus);
+
+    CLOCK__enSetACLKSource(CLOCK_enSOURCE_LFXT1); /*32768 Hz*/
+	CLOCK__enSetMCLKSource(CLOCK_enSOURCE_DCO); /*~8 MHz*/
+    CLOCK__enSetSMCLKSource(CLOCK_enSOURCE_XT2); /*8 MHz*/
+
+    WDT__vRegisterIRQSourceHandler(&MAIN_u16WDTInterval);
+    WDT__vSetConfig(&stWDTConfig);
+
+    TIMERA__vSetModuleValue(TIMERA_enCC0, 20000U - 1U);
+    TIMERA__vSetCompareConfigExt(TIMERA_enCC1, &stCC1Config);
+
+    TIMERA__vSetConfigExt(&stTIMERAConfig);
+    WDT_CTL_R = (WDT_CTL_R & ~WDT_CTL_R_PW_MASK) | WDT_CTL_R_PW_WRITE | WDT_CTL_R_CNTCL_CLEAR;
+
+    GPIO__vRegisterIRQSourceHandler(SWITCH1_PORT, SWITCH1_PIN, &MAIN_u16SWITCH1);
+    GPIO__vSetInterruptEdgeByNumber(SWITCH1_PORT, SWITCH1_PIN, GPIO_enINT_EDGE_FALLING);
+    GPIO__vClearInterruptSourceByNumber(SWITCH1_PORT, SWITCH1_PIN);
+    GPIO__vEnaInterruptSourceByNumber(SWITCH1_PORT, SWITCH1_PIN);
+
+    _EINT();
+	while(1U)
+	{
+
+	}
+}
+
+uint16_t MAIN_u16WDTInterval(uintptr_t ptrBlock, uint8_t u8Source)
+{
+    static int8_t s8Direction = DEGREE_INCREASE;
+    static int16_t s16Degree = 0U;
+    if((s16Degree >= MAX_DEGREE_VALUE) && (s8Direction == DEGREE_INCREASE))
+    {
+        s8Direction = (int8_t) -DEGREE_INCREASE;
+    }
+    else if((s16Degree == 0U) && (s8Direction == (int8_t) -DEGREE_INCREASE))
+    {
+        s8Direction = (int8_t) DEGREE_INCREASE;
+    }
+    s16Degree += s8Direction;
+    u16Degree = (uint16_t) s16Degree;
+
+
+    TIMERA_CC1_R_R = u16Degree + u16DegreeBase - 1U;
+    u16WDTCount++;
+    PORT1_OUT_R ^= PORT_OUT_R_PIN0_MASK;
+    return (0U);
+}
+
+uint16_t MAIN_u16SWITCH1(uintptr_t ptrBlock, uint8_t u8Source)
+{
+    PORT1_OUT_R ^= PORT_OUT_R_PIN2_MASK;
+    return (0U);
+}
+
+
---
+++ MSP430F169_TIMER_SERVO/msp430f169_startup.c
@@ -0,0 +1,57 @@
+/**
+ *
+ * @file msp430f169_startup.c
+ * @copyright
+ * @verbatim InDeviceMex 2021 @endverbatim
+ *
+ * @par Responsibility
+ * @verbatim InDeviceMex Developers @endverbatim
+ *
+ * @version
+ * @verbatim 1.0 @endverbatim
+ *
+ * @date
+ * @verbatim 25 mar. 2022 @endverbatim
+ *
+ * @author
+ * @verbatim InDeviceMex @endverbatim
+ *
+ * @par Change History
+ * @verbatim
+ * Date           Author     Version     Description
+ * 25 mar. 2022     InDeviceMex    1.0         initial Version@endverbatim
+ */
+#include "DriverLib/DriverLib.h"
+
+__interrupt void IntDefaultHandler(void);
+
+#pragma RETAIN (INTERRUPT__pfvVector)
+#pragma DATA_SECTION(INTERRUPT__pfvVector, ".intvecs")
+void (* const INTERRUPT__pfvVector[15U])(void) =
+{
+    &DACDMA_IRQVectorHandler,                /* The DACDMA handler*/
+    &PORT2_IRQVectorHandler,                 /* The PORT2 handler*/
+    &IntDefaultHandler,                      /* The USART1TX handler*/
+    &IntDefaultHandler,                      /* The USART1RX handler*/
+    &PORT1_IRQVectorHandler,                 /* The PORT1 handler*/
+    &TIMERA_IRQVectorHandler,                /* The TIMERA1 handler*/
+    &TIMERA_CC0_IRQVectorHandler,            /* The TIMERA0 handler*/
+    &IntDefaultHandler,                      /* The ADC12 handler*/
+    &IntDefaultHandler,                      /* The USART0TX handler*/
+    &IntDefaultHandler,                      /* The USART0RX handler*/
+    &WDT_IRQVectorHandler,                   /* The WDT handler*/
+    &IntDefaultHandler,                      /* The COMPARATORA handler*/
+    &IntDefaultHandler,                      /* The TIMERB1 handler*/
+    &IntDefaultHandler,                      /* The TIMERB0 handler*/
+    &NMI_IRQVectorHandler,                   /* The NMI handler*/
+};
+
+__interrupt void IntDefaultHandler(void)
+{
+    while(1)
+    {
+
+    }
+}
+
+
