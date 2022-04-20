842a140aa642945551647881e370ddc343e11c64
InDeviceMex <indevicemex@gmail.com>
2022-04-20 12:51:39 -0500

    Actualizar lnk_msp430f169.cmd, main.c y msp430f169_startup.c

---
+++ MSP430F169_ADC_SINGLE/main.c
@@ -0,0 +1,30 @@
+#include "DriverLib/DriverLib.h"
+
+void main(void)
+{
+    CLOCK_nINT_STATUS enClockStatus = CLOCK_enINT_STATUS_NOOCCUR;
+    uint16_t u16Iter = 0U;
+    /** Rosc*/
+    GPIO__vSetConfig(GPIO_enROSC_P25);
+
+    CLOCK__vSetDCOFrequency(800000UL, CLOCK_enRESISTOR_EXTERNAL);
+    CLOCK__vSetLFXT1FrequencyMode(CLOCK_enFREQMODE_LOW);
+    CLOCK__vSetXT2Enable(CLOCK_enENABLE_ENA);
+	do
+	{
+	    CLOCK__vClearInterruptSource();
+	    for(u16Iter = 0U; u16Iter < 4000U; u16Iter++); /*At least 50us*/
+	    enClockStatus = CLOCK__enGetStatusInterruptSource();
+	}while(CLOCK_enINT_STATUS_NOOCCUR != enClockStatus);
+
+    CLOCK__enSetACLKSource(CLOCK_enSOURCE_LFXT1); /*32768 Hz*/
+	CLOCK__enSetMCLKSource(CLOCK_enSOURCE_DCO); /*~8 MHz*/
+    CLOCK__enSetSMCLKSource(CLOCK_enSOURCE_XT2); /*8 MHz*/
+
+    _EINT();
+	while(1U)
+	{
+
+	}
+}
+
